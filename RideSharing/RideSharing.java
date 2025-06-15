6fd63458005848387d6107a33ec172c56543a753
Amit Upadhyay <amit.upadhyay@insurancedekho.com>
2025-06-15 18:17:33 +0530

    fix

--- RideSharing/RideSharing.java
+++ RideSharing/RideSharing.java
@@ -1,7 +1,61 @@
 package RideSharing;
-
 import java.util.*;
+enum AccountStatus{
+    ACTIVE,
+    INACTIVE
+}
+enum AccountMode{
+    RIDER,
+    DRIVER
+}
+class Account{
+    private String username,password;
+    private String mobileNumber;
+    private AccountStatus accountStatus;
+    private AccountMode accountMode;
+}
+class User{
+    private String id;
+    private String displayName;
+    private Account account;
+    User(String displayName,Account account){
+        this.displayName = displayName;
+        this.account = account;
+    }
+}
+interface ISearchRide{
+    void searchRide(Location currentLocation,Location dstLocation,VehicleModel vehicleModel);
+}
+interface IRideAssignmentStrategy{
+    void getMeBestRide(Location currentLocation,Location dstLocation,VehicleModel vehicleModel);
+}
+class ShortestTimeRideStrategy implements IRideAssignmentStrategy{
+    public void getMeBestRide(Location currentLocation,Location dstLocation,VehicleModel vehicleModel){
+        
+    }
+}
+class SearchRide implements ISearchRide{
+    public void searchRide(Location currentLocation,Location dstLocation,VehicleModel vehicleModel){
 
+    }
+}
+interface IRiderModeService{
+    void searchRide();
+    void cancelRide();
+    void completePaymentForRide();
+    // void rideHistory();
+}
+interface IDriverModeService{
+    void acceptRide();
+    void cancelRide();
+}
+class RiderService implements IRiderModeService{
+    private ISearchRide iSearchRide;
+    RiderService(ISearchRide iSearchRide){
+        this.iSearchRide = iSearchRide;
+    }
+
+}
 enum VehicleType{
     TW,
     FW
@@ -30,7 +84,6 @@ class VehicleFactoryProducer {
         }
     }
 }
-
 class TW extends Vehicle{
 }
 class FW extends Vehicle{
@@ -66,7 +119,7 @@ class TWVehicleModel extends VehicleModel{
     public VehicleType getType() { return VehicleType.TW; }
 }
 class FWVehicleModel extends VehicleModel{
-    public VehicleType getType() { return VehicleType.TW; }
+    public VehicleType getType() { return VehicleType.FW; }
 }
 class Bike extends TWVehicleModel{
 
@@ -82,15 +135,33 @@ class Sudan extends FWVehicleModel{
 }
 interface ILocationService{
     void updateLocation(Location location);
-    void assignLocation();
+    void assignLocation(String userId,Location location);
+}
+interface ICacheLocationManager{
+    Location getLocation(String userId);
+    void setLocation(String userId,Location location);
+}
+class CacheManager implements ICacheLocationManager{
+    private final Map<String,Location>mp;
+    CacheManager(){
+        mp = new HashMap<>();
+    }
+    public Location getLocation(String userId){
+        return mp.get(userId);
+    }
+    public void setLocation(String userId,Location location){
+        mp.put(userId, location);
+    }
 }
 class LocationService implements ILocationService{
     private INotificationManager iNotificationManager;
-    LocationService(INotificationManager iNotificationManager){
+    private ICacheLocationManager iCacheLocationManager;
+    LocationService(INotificationManager iNotificationManager,ICacheLocationManager iCacheLocationManager){
         this.iNotificationManager = iNotificationManager;
+        this.iCacheLocationManager = iCacheLocationManager;
     }
-    public void assignLocation(){
-
+    public void assignLocation(String userId,Location location){
+        iCacheLocationManager.setLocation(userId, location);
     }
     public void updateLocation(Location location){
         iNotificationManager.notify(location);
