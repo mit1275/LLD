d1664334835439a5af43c15ba295e65fae1de423
Amit Upadhyay <amit.upadhyay@insurancedekho.com>
2025-06-17 23:44:49 +0530

    atm

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -1,7 +1,17 @@
 package ATM;
 
-import javax.smartcardio.Card;
-
+enum CardType{
+    DEBIT_CARD,
+    CREDIT_CARD
+}
+class Card{
+    private String cardNo;
+    private String name;
+    private String cvv;
+    private Date issuedAt;
+    private Date issuedTill;
+    private CardType cardType;
+}
 interface IATMState{
     void insertCard(Card card);
     void ejectCard(Card card);
@@ -10,28 +20,50 @@ interface IATMState{
     void authenticateUser();
     void startTransaction();
 }
+interface ICardReader{
+    void readCard(Card card);
+}
+class CardReaderFetchIssuerBankService implements ICardReader{
+    public void readCard(Card card){
+
+    }
+}
+class CardReaderFetchBinNumberService implements ICardReader{
+    public void readCard(Card card){
+
+    }
+}
+class CardReaderFetchCardTypeService implements ICardReader{
+    public void readCard(Card card){
+
+    }
+}
 class IdleState implements IATMState{
     private ATM atm;
-    IdleState(ATM atm){
+    private ICardReader cardReader;
+    IdleState(ATM atm,ICardReader cardReader){
         this.atm = atm;
+        this.cardReader = cardReader;
     }
     public void insertCard(Card card){
-
+        System.out.println("Insert card"+card);
+        atm.setState(atm.getHasCardState());
+        cardReader.readCard(card);
     }
     public void ejectCard(Card card){
-
+        System.out.println("Please insert card first");
     }
     public void pressButton(){
-
+        System.out.println("Please insert card first");
     }
     public void enterPin(){
-
+        System.out.println("Please insert card first");
     }
     public void authenticateUser(){
-
+        System.out.println("Please insert card first");
     }
     public void startTransaction(){
-
+        System.out.println("Please insert card first");
     }
 }
 class HasCardState implements IATMState{
@@ -40,13 +72,16 @@ class HasCardState implements IATMState{
         this.atm = atm;
     }
     public void insertCard(Card card){
-
+        System.out.println("Card is already inserted");
+        return ;
     }
     public void ejectCard(Card card){
-
+        System.out.println("Card is ejected");
+        atm.setState(atm.getIdleState());
     }
     public void pressButton(){
-
+        System.out.println("Button is pressed");
+        // atm.setState(null);
     }
     public void enterPin(){
 
@@ -115,13 +150,14 @@ interface IAuthentication{
     void authenticateUser(Card card);
 }
 class ATM{
+    private ICardReader cardReader;
     private IATMState IdleState;
     private IATMState HasCardState;
     private IATMState AuthenticateUserState;
     private IATMState TransactionState;
     private IATMState curState;
     ATM(){
-        IdleState = new IdleState(this);
+        IdleState = new IdleState(this,cardReader);
         HasCardState = new HasCardState(this);
         AuthenticateUserState = new AuthenticateUserState(this);
         TransactionState = new TransactionState(this);
@@ -146,10 +182,10 @@ class ATM{
         return curState;
     }
     public void insertCard(Card card){
-        curState.insertCard(Card card);
+        curState.insertCard(card);
     }
     public void ejectCard(Card card){
-        curState.ejectCard(Card card);
+        curState.ejectCard(card);
     }
     public void pressButton(){
         curState.pressButton();
